055c422578837bf08e85949a3d2f7b9d4edd4204
wandilemawelela <wandile1997@gmail.com>
2024-12-08 22:36:12 -0800

    [ADD] Completed task 3: Add a new user to our database using our API

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const emailExists = await dbClient.db.collection('users').findOne({ email });
+    
+    if (emailExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = { email, password: hashedPassword };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-// routes/index.js
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
