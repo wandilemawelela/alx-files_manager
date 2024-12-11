6c22fc8552823ffc7c58df897516406af3de257c
wandilemawelela <wandile1997@gmail.com>
2024-12-11 12:14:20 -0800

    [ADD] Add background processing for sending welcome emails to new users

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
+const Bull = require('bull');
+
+// Create userQueue for sending welcome emails
+const userQueue = new Bull('userQueue');
 
 const router = express.Router();
 
@@ -18,5 +22,15 @@ router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = await UsersController.createUser(req, res);
+    // Add a job to the userQueue for the new user
+    await userQueue.add({ userId: user._id });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
 
 module.exports = router;
--- worker.js
+++ worker.js
@@ -1,61 +1,52 @@
-import { fileQueue } from './queues/fileQueue';  // Import the Bull queue
-import imageThumbnail from 'image-thumbnail';
-import { ObjectId } from 'mongodb';
-import fs from 'fs';
-import dbClient from './utils/db';  // MongoDB client
-import path from 'path';
+const Bull = require('bull');
+const dbClient = require('./utils/dbClient');
 
+// Create fileQueue for generating thumbnails
+const fileQueue = new Bull('fileQueue');
+
+// Create userQueue for sending welcome emails
+const userQueue = new Bull('userQueue');
+
+// Process fileQueue for generating thumbnails
 fileQueue.process(async (job) => {
-  const { userId, fileId } = job.data;
+  const { fileId, userId } = job.data;
 
-  // Ensure fileId and userId are present
   if (!fileId) {
     throw new Error('Missing fileId');
   }
+
   if (!userId) {
     throw new Error('Missing userId');
   }
 
-  // Retrieve the file from the database
-  const file = await dbClient.db.collection('files').findOne({
-    _id: ObjectId(fileId),
-    userId: ObjectId(userId),
-  });
+  const fileDoc = await dbClient.findFileById(fileId);
 
-  // If no file is found for the given userId and fileId, raise an error
-  if (!file) {
+  if (!fileDoc || fileDoc.userId !== userId) {
     throw new Error('File not found');
   }
 
-  // If the file is not an image, raise an error
-  if (file.type !== 'image') {
-    throw new Error('File is not an image');
+  const thumbnails = [500, 250, 100].map(async (size) => {
+    const thumbnail = await generateThumbnail(fileDoc.localPath, { width: size });
+    await saveThumbnail(fileDoc.localPath, thumbnail, size);
+  });
+
+  await Promise.all(thumbnails);
+});
+
+// Process userQueue for sending welcome emails
+userQueue.process(async (job) => {
+  const { userId } = job.data;
+
+  if (!userId) {
+    throw new Error('Missing userId');
   }
 
-  const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
+  const userDoc = await dbClient.findUserById(userId);
 
-  // Ensure the folder exists
-  if (!fs.existsSync(FOLDER_PATH)) {
-    fs.mkdirSync(FOLDER_PATH, { recursive: true });
+  if (!userDoc) {
+    throw new Error('User not found');
   }
 
-  // Thumbnail sizes (500px, 250px, and 100px)
-  const thumbnailSizes = [500, 250, 100];
-
-  for (const size of thumbnailSizes) {
-    const thumbnailPath = path.join(FOLDER_PATH, `${fileId}_${size}${path.extname(file.name)}`);
-
-    try {
-      // Generate the thumbnail with the specified size
-      const options = { width: size };
-      const thumbnail = await imageThumbnail(file.localPath, options);
-
-      // Save the thumbnail to the local filesystem
-      fs.writeFileSync(thumbnailPath, thumbnail);
-      console.log(`Generated thumbnail at ${thumbnailPath}`);
-    } catch (error) {
-      console.error(`Error generating thumbnail for ${fileId}:`, error);
-    }
-  }
+  console.log(`Welcome ${userDoc.email}!`);
 });
 
